1d77e94c825f4e192e7a4ccf98da49a7e98b358e
babymilooo <Kyrylo.Yehorov@cit.khpi.edu.ua>
2024-04-09 13:40:41 +0300

    Features: add skeleton to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 import { RootStoreContext } from '@/providers/rootStoreProvider';
 import { observer } from 'mobx-react-lite';
-
+import { Skeleton } from "@/components/ui/skeleton"
 import {
     Popover as MyPopover,
     PopoverContent as MyPopoverContent,
@@ -65,71 +65,62 @@ const Navbar = ({ data }) => {
                     </div>
                 </div>
                 <div className='flex flex-1 items-center space-x-2 justify-end'>
-                    loading...
+                    <div className="flex items-center space-x-4">
+                        <Skeleton className="h-6 w-6 rounded-full" />
+                    </div>
                 </div>
             </div>
         </header >;
     }
 
     return (
-
-        !userStore.isLoggedIn ? (
-
-            < header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60" >
-                <div className="container flex h-14 max-w-screen-2xl items-center">
-                    <div className="hidden ipad:flex">
-                        <div className="text-xl text-foreground flex items-center cursor-pointer" onClick={() => router.push("/home")}>
-                            <Image src="/bigLogo.png" alt='logo' height={40} width={35} />
-                            <Image src="/logoWord.png" alt='word' height={40} width={100} />
-                        </div>
-                    </div>
-                    <div className='flex flex-1 items-center space-x-2 justify-end'>
-                        <Button variant="ghost" onClick={() => router.push("/auth/login")}>login</Button>
-                        <button className=" bg-lime-400 px-6 py-3 rounded-3xl font-bold text-xs" onClick={() => router.push("/auth/registration")} >
-                            Registration
-                        </button>
+        <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60" >
+            <div className="container flex h-14 max-w-screen-2xl items-center">
+                <div className="hidden ipad:flex">
+                    <div className="text-xl text-foreground flex items-center cursor-pointer" onClick={() => router.push("/home")}>
+                        <Image src="/bigLogo.png" alt='logo' height={40} width={35} />
+                        <Image src="/logoWord.png" alt='word' height={40} width={100} />
                     </div>
                 </div>
-            </header >
-        ) : (
-            < header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60" >
-                <div className="container flex h-14 max-w-screen-2xl items-center">
-                    <div className="hidden ipad:flex">
-                        <div className="text-xl text-foreground flex items-center cursor-pointer" onClick={() => router.push("/home")}>
-                            <Image src="/bigLogo.png" alt='logo' height={40} width={35} />
-                            <Image src="/logoWord.png" alt='word' height={40} width={100} />
-                        </div>
-                    </div>
-                    <div className='flex flex-1 items-center space-x-2 justify-end cursor-pointer'>
-                        <MyPopover>
-                            <MyPopoverTrigger>
-                                <Tooltip text={userStore.user?.userName}>
-                                    <Avatar>
-                                        <AvatarImage src={userStore.user?.image} alt="@avatar" className="w-[100px]" />
-                                        <AvatarFallback>CN</AvatarFallback>
-                                    </Avatar>
-                                </Tooltip>
-                            </MyPopoverTrigger>
-                            <MyPopoverContent className="rounded-lg">
-                                <div className="flex flex-col font-bold">
-                                    <div className="pt-4 pl-4 cursor-pointer">
-                                        account
-                                    </div>
-                                    <div className="p-4 cursor-pointer ">
-                                        settings
-                                    </div>
-                                    <div className="w-full border-t">
-                                        <p className="p-4 cursor-pointer" onClick={handleLogout}>
-                                            Logout
-                                        </p>
-                                    </div>
-                                </div>
-                            </MyPopoverContent>
-                        </MyPopover>
-                    </div>
-                </div>
-            </header >)
 
+                {
+                    !userStore.isLoggedIn ? (
+                        <div className='flex flex-1 items-center space-x-2 justify-end'>
+                            <Button variant="ghost" onClick={() => router.push("/auth/login")}>login</Button>
+                            <button className=" bg-lime-400 px-6 py-3 rounded-3xl font-bold text-xs" onClick={() => router.push("/auth/registration")} >
+                                Registration
+                            </button>
+                        </div>) : (
+                        <div className='flex flex-1 items-center space-x-2 justify-end cursor-pointer'>
+                            <MyPopover>
+                                <MyPopoverTrigger>
+                                    <Tooltip text={userStore.user?.userName}>
+                                        <Avatar>
+                                            <AvatarImage src={userStore.user?.image} alt="@avatar" className="w-[100px]" />
+                                            <AvatarFallback>CN</AvatarFallback>
+                                        </Avatar>
+                                    </Tooltip>
+                                </MyPopoverTrigger>
+                                <MyPopoverContent className="rounded-lg">
+                                    <div className="flex flex-col font-bold">
+                                        <div className="pt-4 pl-4 cursor-pointer">
+                                            account
+                                        </div>
+                                        <div className="p-4 cursor-pointer ">
+                                            settings
+                                        </div>
+                                        <div className="w-full border-t">
+                                            <p className="p-4 cursor-pointer" onClick={handleLogout}>
+                                                Logout
+                                            </p>
+                                        </div>
+                                    </div>
+                                </MyPopoverContent>
+                            </MyPopover>
+                        </div>)
+                }
+            </div>
+        </header >
     );
 };
 
