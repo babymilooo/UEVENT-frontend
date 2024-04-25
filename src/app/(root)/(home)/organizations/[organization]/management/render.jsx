ae720c8cde6ddd76650e550610eea315646e719b
babymilooo <Kyrylo.Yehorov@cit.khpi.edu.ua>
2024-04-25 16:40:27 +0300

    Refactor: main and openLeftbar

--- src/app/(root)/(home)/organizations/[organization]/management/render.jsx
+++ src/app/(root)/(home)/organizations/[organization]/management/render.jsx
@@ -29,14 +29,7 @@ import {
     DialogTrigger,
 } from "@/components/ui/dialog"
 
-import {
-    Sheet,
-    SheetContent,
-    SheetDescription,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger,
-} from "@/components/ui/sheet"
+
 
 import {
     Popover,
@@ -59,6 +52,10 @@ import ImageLoader from '@/components/ImageLoader/ImageLoader';
 import OrganizationService from '@/service/orgService';
 import { useRouter } from 'next/navigation';
 import { API_URL } from '@/https/axios';
+
+import { Main } from './main';
+import { OpenLeftBar } from './openLeftBar';
+import CreateNew from './createNew';
 
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
@@ -154,7 +151,7 @@ const Render = ({ res }) => {
 
     const handleClose = async () => {
         console.log("close");
-        setSelectedImage (organization.logo);
+        setSelectedImage(organization.logo);
         setBackgroundImage(organization.picture);
         Setname(organization.name);
         SetDescription(organization.description);
@@ -170,158 +167,22 @@ const Render = ({ res }) => {
     return (
         <div className='grid grid-cols-4 bg-muted h-full'>
             <div className=" lg:col-span-3 col-span-4 rounded-md mt-2 flex flex-col">
-                <div className='ipad:px-5 ipad:pt-20 pt-5 ipad:pb-5 items-center flex flex-col ipad:flex-row w-full'>
-                    <Image src={organization.logo ? organization.logo : "/BigLogo.png"} alt='logo' height={200} width={200} className='rounded-lg h-[200px]' style={{
-                        objectFit: 'cover',
-                        objectPosition: 'center'
-                    }} />
-                    <div className='ipad:pl-5 flex-col'>
-                        {isVerified && (
-                            <div className="hidden ipad:flex items-center none">
-                                <Image src="/verified.svg" alt="verified" width={20} height={20} />
-                                <div className="text-primary">Verified</div>
-                            </div>
-                        )}
-
-                        <h1 className='iphone:text-6xl text-5xl font-bold pt-5 ipad:pt-0'>{organization.name}</h1>
-
-                        <div className='flex items-center ipad:justify-normal justify-between ipad:mt-16 mb-5 ipad:mb-0 gap-1 '>
-                            <div className='flex items-center gap-1'>
-                                <p className='text-xl font-bold'>{organization.followerCount}</p>
-                                <p className='font-bold text-xs pt-1'>Followers</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className='lg:hidden fixed right-0'>
-                    <Sheet>
-                        <SheetTrigger>Open</SheetTrigger>
-                        <SheetContent>
-                            <div className="flex flex-col fixed h-full">
-                                <div className="bg-background h-full rounded-md">
-                                    <ScrollArea className="h-full w-full rounded-md">
-                                        <div className="relative flex h-[360px] w-full items-end bg-cover bg-center select-none rounded-t-md"
-                                            style={{
-                                                backgroundImage: `url('/gradient.jpeg')`,
-                                            }}>
-                                            <div className="absolute bottom-0 left-0 w-full h-[200px] bg-gradient-to-t from-black to-transparent"></div>
-                                        </div>
-                                        <div className="relative w-full mt-[-35px] bg-background z-30 rounded-[40px]">
-                                            <div className="flex items-center gap-2 p-4 pb-0">
-                                                <Avatar>
-                                                    <AvatarImage src="/BigLogo.png" alt="@avatar" className="w-[50px]" />
-                                                    <AvatarFallback>CN</AvatarFallback>
-                                                </Avatar>
-                                                <div></div>
-                                                <div className="font-bold">
-                                                    Organization
-                                                </div>
-
-                                                {isVerified && (
-                                                    <div className="hidden ipad:flex items-center none">
-                                                        <Image src="/verified.svg" alt="verified" width={20} height={20} />
-                                                    </div>
-                                                )}
-                                            </div>
-                                            <div className="mt-4 p-1">
-                                                <div className="font-bold">Hard Rock Stadium, Miami Gardens, US</div>
-                                                <p className="font-bold mt-4 text-sm">
-                                                    About us
-                                                </p>
-                                                <p className=' text-muted-foreground text-sm'>     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam, at dignissimos deleniti commodi adipisci beatae aperiam saepe harum. Incidunt doloribus quibusdam aspernatur reiciendis quod vel numquam! Officiis sunt aliquid rem.
-                                                </p>
-                                                <p className="font-bold mt-4 mb-2 text-sm">
-                                                    Events
-                                                </p>
-                                                {Array.from({ length: 3 }).map((_, index) => (
-
-                                                    <Card key={index}
-                                                        className="relative flex w-full items-end bg-cover bg-center select-none overflow-hidden h-[80px] mb-1"
-                                                        style={{
-                                                            backgroundImage: `url('/rolingLoud.webp')`,
-                                                        }}>
-                                                        <div className="absolute inset-0 bg-black opacity-50"></div>
-                                                        <CardContent className="flex items-center h-full w-full">
-                                                            <div className="bg-neutral-800 w-[75px] rounded-md h-full flex ">
-                                                                <div className="flex flex-col items-center justify-center w-full h-full z-10">
-                                                                    <p className="font-bold text-white">May</p>
-                                                                    <p className="text-4xl font-bold text-white">18</p>
-
-                                                                </div>
-                                                            </div>
-                                                            <div className="flex flex-col">
-                                                                <p className="text-sm font-bold ml-2 text-white z-10">Location</p>
-                                                                <p className="text-sm font-bold ml-2 text-white z-10">Name</p>
-                                                                <p className="text-sm font-bold ml-2 text-white z-10">Mon. 17:00</p>
-                                                            </div>
-                                                        </CardContent>
-                                                    </Card>
-                                                ))}
-                                            </div>
-                                        </div>
-                                    </ScrollArea>
-                                </div>
-                            </div>
-                        </SheetContent>
-                    </Sheet>
-                </div>
+                <Main isVerified={isVerified} organization={organization} />
+                <OpenLeftBar isVerified={isVerified} organization={organization} />
                 <div className='bg-background rounded-t-md h-full ipad:p-5'>
                     <div className='flex justify-center items-center w-full gap-2 border-b pb-5'>
-                        <Dialog>
-                            <DialogTrigger className=" bg-lime-400 px-6 py-1 rounded-3xl font-bold text-xs">
-                                Create new
-                            </DialogTrigger>
-                            <DialogOverlay /> 
-                            <DialogContent className="max-w-[1000px] ">
-                                <DialogHeader>
-                                    <DialogTitle>New event</DialogTitle>
-                                </DialogHeader>
-                                <div className="grid grid-cols-2 gap-4">
-                                    <div className='col-span-1'>
-                                        <div className='flex flex-col gap-4 items-center'>
-
-                                            <div className="relative flex h-[200px] w-full items-end bg-cover bg-center select-none overflow-hidden"
-                                                style={{
-                                                    backgroundImage: `url('${backgroundImage}')`,
-                                                }}>
-                                                <div className="absolute bottom-0 left-0 w-full h-[200px] bg-gradient-to-t from-black to-transparent"></div>
-                                                <label htmlFor="background-image-upload" className="absolute inset-0 cursor-pointer">
-                                                    <input
-                                                        id="background-image-upload"
-                                                        type="file"
-                                                        className="absolute inset-0 h-full opacity-0 cursor-pointer"
-                                                        onChange={handleImageChange}
-                                                        accept="image/*"
-                                                    />
-                                                </label>
-                                            </div>
-
-                                            <div className="relative w-full p-6 mt-[-50px] bg-background z-30 rounded-[40px]">
-                                                <div className='flex items-center gap-4'>
-                                                    <Input onChange={(e) => handleNameChange(e)} placeholder="organization name" />
-                                                </div>
-                                                <Textarea placeholder="Description" className="mt-4" onChange={(e) => handleDescriptionChange(e)} />
-
-                                                <div className='flex items-center gap-4 mt-4 border-t pt-4'>
-                                                    <Label>Choose start date</Label>
-                                                    <DatePicker date={endDate} setDate={setEndDate} />
-                                                    <MyTimePicker time={startTime} setTime={setStartTime} />
-                                                </div>
-
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className='col-span-1'>
-                                        <GoogleMap selectedPlace={selectedPlace} setSelectedPlace={setSelectedPlace} />
-                                    </div>
-                                    <DialogClose className='col-span-2 justify-end grid grid-cols-3'>
-                                        <div></div>
-                                        <div></div>
-                                        <Button onClick={handleCreate} className="col-span-1">New</Button>
-                                    </DialogClose>
-                                </div>
-                            </DialogContent>
-                        </Dialog>
+                        <CreateNew
+                            backgroundImage={backgroundImage}
+                            handleImageChange={handleImageChange}
+                            handleNameChange={handleNameChange}
+                            handleDescriptionChange={handleDescriptionChange}
+                            endDate={endDate}
+                            setEndDate={setEndDate}
+                            startTime={startTime}
+                            setStartTime={setStartTime}
+                            selectedPlace={selectedPlace}
+                            setSelectedPlace={setSelectedPlace}
+                            handleCreate={handleCreate} />
 
                         <Input
                             placeholder='Search organization'
@@ -468,7 +329,7 @@ const Render = ({ res }) => {
                                     {Array.from({ length: 3 }).map((_, index) => (
 
                                         <Card
-                                        key={index}
+                                            key={index}
                                             className="relative flex w-full items-end bg-cover bg-center select-none overflow-hidden h-[80px] mb-1"
                                             style={{
                                                 backgroundImage: `url('/rolingLoud.webp')`,
