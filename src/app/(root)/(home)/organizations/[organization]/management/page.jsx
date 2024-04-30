e0bd1189105b90f500c3eb2b5af9fb7ad472a869
DarunkaKa <shmetterling.dk@gmail.com>
2024-04-30 19:12:48 +0300

    pagination in events

--- src/app/(root)/(home)/organizations/[organization]/management/page.jsx
+++ src/app/(root)/(home)/organizations/[organization]/management/page.jsx
@@ -1,32 +1,23 @@
 import React from 'react';
 import Render from './management';
 import OrganizationService from '@/service/orgService';
-import EventService from '@/service/eventService';
 
 export default async function page({ params }) {
     const response = await getServerSideProps(params);
-    const { evdata, org, totalItems, totalPages } = response;
+    const { org } = response;
     return (
         <div className="xl:pl-[250px] lg:pl-[200px] flex flex-col h-full overflow-hidden">
-            <Render res={org} eventsData={evdata} total={totalItems} totalPages={totalPages} />
+            <Render res={org} />
         </div>
     );
 };
 
 async function getServerSideProps(params) {
     const res = await OrganizationService.getOrganization(params.organization);
-    const result = await EventService.getEvents(res.data._id, params.limit, params.currentPage);
-
-    if (!result)
+    if (!res)
         return null;
     const org = res.data;
-    const evdata = result.data.events;
-    const totalItems = result.data.totalItems;
-    const totalPages = result.data.totalPages;
     return {
-        evdata,
-        org,
-        totalItems,
-        totalPages
+        org
     };
 }
