6b6b93081c74171e05d8867e2adc13ce67e5212e
babymilooo <Kyrylo.Yehorov@cit.khpi.edu.ua>
2024-05-02 14:34:25 +0300

    Fix: display events on home page

--- src/app/(root)/(home)/home/page.jsx
+++ src/app/(root)/(home)/home/page.jsx
@@ -18,6 +18,8 @@ import { useRouter } from 'next/navigation';
 import { observer } from 'mobx-react-lite';
 import { RootStoreContext } from '@/providers/rootStoreProvider';
 import EventService from '@/service/eventService';
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+const days = ['Sun.', 'Mon.', 'Tue.', 'Wed.', 'Thu.', 'Fri.', 'Sat.'];
 
 const page = observer(() => {
     const router = useRouter();
@@ -28,7 +30,24 @@ const page = observer(() => {
     useEffect(() => {
         const fetchUpcoming = async () => {
             const resp = await EventService.getUpcomingEvents();
-            setUpcomingEvents(resp.data.events);
+            const updatedEvents = await Promise.all(
+                resp.data.events.map(async (event) => {
+                    const eventDate = new Date(event.date);
+                    const month = months[eventDate.getMonth()];
+                    const dayOfWeek = days[eventDate.getDay()];
+                    const dayOfMonth = eventDate.getDate();
+
+                    return {
+                        ...event,
+                        month,
+                        dayOfWeek,
+                        dayOfMonth,
+                    };
+                })
+            );
+
+            console.log(updatedEvents)
+            setUpcomingEvents(updatedEvents);
         }
         fetchUpcoming();
     }, [])
@@ -83,21 +102,32 @@ const page = observer(() => {
                     <CarouselPrevious />
                     <CarouselNext />
                 </Carousel>
-                <p className="font-bold text-3xl p-4 select-none">Upcoming events</p>
-                <div className="grid ipad:grid-cols-2 grid-cols-1 gap-8 p-1">
-                    {Array.from({ length: 3 }).map((_, index) => (
-                        <div onClick={() => router.push(`/events/${index}`)} key={index}>
-                            <Card className="h-[360px] cursor-pointer" >
-                                <CardContent className="flex p-6 h-full bg-cover bg-center rounded-md items-center select-none"
-                                    style={{
-                                        backgroundImage: `url('/carti.jpg')`,
-                                    }}
-                                >
-                                    <p className="font-bold xl:text-6xl lg:text-5xl ipad:text-4xl phone:text-xl text-white">get tickets to the best <br /> concerts in the world</p>
-                                </CardContent>
-                            </Card>
+                <p className="font-bold text-3xl p-4 select-none">Upcoming events in</p>
+                <div className="grid ipad:grid-cols-2 grid-cols-1 p-1 gap-2 2xl:grid-cols-3 items-center">
+                    {upcomingEvents?.map((event, index) => (
 
-                        </div>
+                        <Card key={index} className="relative flex col-span-1 items-end bg-cover bg-center select-none overflow-hidden h-[200px] mb-1 cursor-pointer"
+                            style={{ backgroundImage: `url('${event.picture ? event.picture : "/gradient.jpeg"}` }}
+                            onClick={() => (router.push(`/events/${event._id}`))}
+                        >
+                            <div className="absolute inset-0 bg-black opacity-60"></div>
+                            <CardContent className="flex h-full w-full">
+                                <div className="bg-neutral-800 w-[200px] rounded-md h-[200px] flex ">
+                                    <div className="flex flex-col items-center justify-center w-full h-full z-10">
+                                        <p className="font-bold text-5xl text-white">{event.month}</p>
+                                        <p className="text-6xl font-bold text-white">{event.dayOfMonth}</p>
+
+                                    </div>
+                                </div>
+                                <div className="flex flex-col justify-between">
+                                    <p className=" font-bold ml-2 text-xl text-white mt-16 z-10">{event.name}</p>
+                                    <div className='flex flex-col mb-8'>
+                                        <p className="font-bold ml-2 text-white text-lg z-10 text-wrap">{event.location.address}</p>
+                                        <p className=" font-bold ml-2 text-white z-10">{event.dayOfWeek} {event.time}</p>
+                                    </div>
+                                </div>
+                            </CardContent>
+                        </Card>
                     ))}
                 </div>
             </div>
